a4b88d31f523a54ef84c5950b06458987d5328be
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-06-12 14:29:12 +0530

    Added scoreboard component

--- src/components/game-panel/scoreboard/ScoreBoard.jsx
+++ src/components/game-panel/scoreboard/ScoreBoard.jsx
@@ -14,12 +14,18 @@ function ScoreBoard() {
   }, [gameContext.leftPlayerScore, gameContext.rightPlayerScore]);
 
   return (
-    <div className="flex items-center justify-between w-full h-16">
-      <div className="font-score text-white pl-4">
-        {gameContext.leftPlayerScore}
+    <div className="flex items-center justify-between w-full h-24">
+      <div className="flex flex-col items-center gap-5 pl-4">
+        <div className="text-white text-2xl">{gameContext.leftPlayerName}</div>
+        <div className="font-score text-white">
+          {gameContext.leftPlayerScore}
+        </div>
       </div>
-      <div className="font-score text-white pr-4">
-        {gameContext.rightPlayerScore}
+      <div className="flex flex-col items-center gap-5 pr-4">
+        <div className="text-white text-2xl">{gameContext.rightPlayerName}</div>
+        <div className="font-score text-white">
+          {gameContext.rightPlayerScore}
+        </div>
       </div>
     </div>
   );
