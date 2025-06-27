a9d5ce67d1b347bacdb027cb4e8751705a9426b7
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-06-27 18:55:21 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,24 @@ import MainPanel from "./components/MainPanel";
 import HeaderImage from "./images/header.png";
 import SimulatorContext from "./store/simulatorContext";
 import SimulatorPanel from "./components/simulator/SimulatorPanel";
+import Footer from "./components/ui/Footer";
 
 function App() {
   const simulatorContext = useContext(SimulatorContext);
 
   return (
+    <div>
     <div className="app p-4 bg-dark-blue min-h-screen app-grid">
       <div className="flex items-center justify-center mb-4">
         <img src={HeaderImage} alt="PongX" className="w-full img-fit" />
       </div>
-      {simulatorContext.simulatorStatus === null ? <MainPanel /> : <SimulatorPanel />}
+      {simulatorContext.simulatorStatus === null ? (
+        <MainPanel />
+      ) : (
+        <SimulatorPanel />
+      )}
+    </div>
+    <Footer />
     </div>
   );
 }
